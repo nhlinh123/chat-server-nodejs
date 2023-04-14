b015369adfd81dd18e844687209d74194e05bfb7
nhlinh123 <nhlinh123@gmail.com>
2023-04-14 16:51:54 +0700

    add env model schema auth

--- config/redis.js
+++ config/redis.js
@@ -1,51 +1,3 @@
-// import { createClient } from 'redis';
-//
-//
-//
-// const redisUrl = `redis://localhost:6379`;
-// const redisClient = createClient({
-//     url: redisUrl,
-// });
-//
-// const connectRedis = async () => {
-//     try {
-//         await redisClient.connect();
-//         console.log('Redis client connected...');
-//     } catch (err: any) {
-//         console.log(err.message);
-//         setTimeout(connectRedis, 5000);
-//     }
-// };
-//
-// connectRedis();
-//
-// redisClient.on('error', (err) => console.log(err));
-//
-// export default redisClient;
-//
-// import { createClient } from 'redis';
-//
-// const redisUrl = `redis://localhost:6379`;
-// const redisClient = createClient({
-//     url: redisUrl,
-// });
-//
-// const connectRedis = async () => {
-//     try {
-//         await redisClient.connect();
-//         console.log('Redis client connected...');
-//     } catch (err: any) {
-//         console.log(err.message);
-//         setTimeout(connectRedis, 5000);
-//     }
-// };
-//
-// connectRedis();
-//
-// redisClient.on('error', (err) => console.log(err));
-//
-// export default redisClient;
-//
 const redis = require('redis');
 
 const redisUrl = `redis://localhost:6379`;
@@ -60,7 +12,7 @@ const connectRedis = async function() {
     } catch (e) {
         console.log(e);
         console.log('Retry connect in 5s');
-        setTimeout(() => connectRedis(), 5000);
+        setTimeout(() => connectRedis(), process.env.RETRY_TIME);
     }
 }
 
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,5 +3,30 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user.model');
 
 class AuthService {
-    static
+    static async authenticationUser (email, password) {
+        const user = await User.findOne({email})
+        if (!user) {
+            return null;
+        }
+
+        try {
+            const publicKey = process.env.PUBLIC_KEY;
+            const token = AuthService.generateToken(user);
+            const decoded = jwt.verify(token, publicKey, { algorithms: ['RS256'] });
+            console.log(decoded); // { userId: '1234567890' }
+        } catch (err) {
+            console.error(err);
+        }
+
+        const isPasswordValid = await  bcrypt.compare(password, user.password);
+        if (!isPasswordValid) return null;
+
+        return user;
+    }
+
+    static generateToken(user) {
+        const privateKey = process.env.PRIVATE_KEY;
+        const token = jwt.sign(user._id, privateKey, {algorithm: 'SHA256'});
+        return token;
+    }
 }
